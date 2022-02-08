53415b1f859b675ed61a4f309fa73db6ee746695
Jina Kim <fghjiurt@naver.com>
2022-02-08 11:32:12 +0900

    signup url 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import GuidePage from 'pages/GuidePage';
 import LoginPage from 'pages/LoginPage';
+import SignupPage from 'pages/SignupPage';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Test from 'Test';
 
@@ -12,6 +13,7 @@ function App() {
 
           <Route path="/test/" element={<Test />} />
           <Route path="/accounts/login/" element={<LoginPage />} />
+          <Route path="/accounts/signup/" element={<SignupPage />} />
           <Route path="/guide/" element={<GuidePage />} />
         </Routes>
       </div>
