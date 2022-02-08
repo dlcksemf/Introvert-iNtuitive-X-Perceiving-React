ce248f5a0bd68c6331b1b64a4dc2c72cde958d30
Jina Kim <fghjiurt@naver.com>
2022-02-08 11:10:13 +0900

    auth 설정 수정 및 authprovider 추가

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from 'base/ErrorBoundary';
+import { AuthProvider } from 'base/hooks/Authcontext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <AuthProvider>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ErrorBoundary>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
