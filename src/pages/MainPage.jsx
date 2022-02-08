c0d902d2964f17766d422b2e8d7d6e74fe07ea4d
Jina Kim <fghjiurt@naver.com>
2022-02-08 13:45:38 +0900

    mainpage 기초설정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,14 @@
-import Main from 'components/Main';
+import HeavyReader from 'components/main/HeavyReader';
+import NewBook from 'components/main/NewBook';
+import Top5 from 'components/main/Top5';
 
 function MainPage() {
   return (
     <div>
       <h2>Main Page</h2>
-      <Main />
+      <Top5 />
+      <HeavyReader />
+      <NewBook />
     </div>
   );
 }
