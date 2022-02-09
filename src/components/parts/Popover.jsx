4186694fff7d4d6387ec0637c67b03b3c46c2767
Chrissy Lee <22chandl@gmail.com>
2022-02-09 11:35:26 +0900

    Top Nav 구체화

---
+++ src/components/parts/Popover.jsx
@@ -0,0 +1,118 @@
+import { Popover, Transition } from '@headlessui/react';
+import { useAuth } from 'base/hooks/Authcontext';
+import { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LOGGED_IN = [
+  {
+    name: 'Logout',
+  },
+  {
+    name: 'Application',
+    url: '/books/application/',
+  },
+  {
+    name: 'OO',
+    url: '##',
+  },
+];
+
+const LOGGED_OUT = [
+  {
+    name: 'Login',
+    url: '/accounts/login/',
+  },
+  {
+    name: 'Signup',
+    url: '/accounts/signup/',
+  },
+];
+
+function PopOver() {
+  const [auth, , , logout] = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  return (
+    <div>
+      <Popover className="relative inline-block">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={`
+                ${open ? '' : 'text-opacity-90'}
+                py-2 inline-flex items-center`}
+            >
+              <svg
+                style={{ width: 30 + 'px', height: 30 + 'px' }}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  className="fill-slate-600"
+                  d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"
+                />
+              </svg>
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-10 w-48 max-w-sm px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0 lg:max-w-3xl">
+                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                  <div className="relative grid gap-8 bg-white p-7">
+                    {auth.isLoggedIn &&
+                      LOGGED_IN.map((item) => (
+                        <div
+                          key={item.name}
+                          onClick={() => {
+                            if (item.name == 'Logout') {
+                              handleLogout();
+                              return;
+                            }
+                            navigate(`${item.url}`);
+                          }}
+                          className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                        >
+                          <div className="ml-4">
+                            <p className="text-sm font-medium text-gray-900">
+                              {item.name}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    {!auth.isLoggedIn &&
+                      LOGGED_OUT.map((item) => (
+                        <div
+                          key={item.name}
+                          onClick={() => {
+                            navigate(`${item.url}`);
+                          }}
+                          className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                        >
+                          <div className="ml-4">
+                            <p className="text-sm font-medium text-gray-900">
+                              {item.name}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </div>
+  );
+}
+
+export default PopOver;
--- src/components/parts/TopNav.jsx
+++ src/components/parts/TopNav.jsx
@@ -1,12 +1,16 @@
-function TopNav({
-  is_staff = false,
-  is_main = true,
-  openNavigator,
-  is_logged_in = true,
-}) {
-  const handleGotoMainPage = () => {
-    if (!is_staff) {
-      console.log('I am Not Staff!');
+// hamburger 바 만들기 & 스타일링 (중앙정렬)
+
+import { useAuth } from 'base/hooks/Authcontext';
+import { useNavigate } from 'react-router-dom';
+import PopOver from './Popover';
+
+function TopNav({ is_main = true }) {
+  const navigate = useNavigate();
+  const [auth] = useAuth();
+
+  const handleGoToMainPage = () => {
+    if (!auth.is_staff) {
+      navigate('/books/booklist');
     }
   };
 
@@ -14,51 +18,49 @@ function TopNav({
     console.log('This is mainPage');
   };
 
-  const handleOpenDropbox = () => {
-    !is_logged_in
-      ? console.log('Not Logged-In')
-      : console.log('This is Logged-In');
-  };
-
   return (
     <>
-      <div className="bg-gray-200 py-2 px-10">
-        <ul className="flex justify-between">
-          {is_main ? (
-            <li className="mr-3">
+      <div className="bg-gray-200 py-2 px-10 h-[64px] w-full">
+        <div className="flex justify-items-center justify-between items-center h-full mx-10">
+          <div>
+            {is_main ? (
               <svg
-                className="w-10 h-10 py-2 cursor-pointer"
+                style={{ width: 30 + 'px', height: 30 + 'px' }}
+                viewBox="0 0 24 24"
                 onClick={handleOpenNavigator}
               >
                 <path
-                  fill="currentColor"
+                  className="fill-slate-600"
                   d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"
                 />
               </svg>
-            </li>
-          ) : (
-            <div className="w-10 h-10 py-2" />
-          )}
-          <li className="mr-3">
+            ) : (
+              <div className="w-10 h-full py-2" />
+            )}
+          </div>
+
+          <div>
             <div
               className="inline-block text-blue-500 text-bold py-2 cursor-pointer"
-              onClick={handleGotoMainPage}
+              onClick={handleGoToMainPage}
             >
               EUCLID SOFT
             </div>
-          </li>
-          <li className="mr-3">
-            <svg
-              className="w-10 h-10 py-2 cursor-pointer"
-              onClick={handleOpenDropbox}
-            >
-              <path
-                fill="currentColor"
-                d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"
-              />
-            </svg>
-          </li>
-        </ul>
+          </div>
+
+          <div>
+            <div className="flex items-center h-full">
+              {auth.isLoggedIn && (
+                <div className="text-[16px] font-bold mr-5 py-2 inline-block">
+                  {auth.is_staff
+                    ? `WELCOME STAFF`
+                    : `WELCOME ${auth.username ? auth.username : 'NoNamer'}`}
+                </div>
+              )}
+              <PopOver />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
