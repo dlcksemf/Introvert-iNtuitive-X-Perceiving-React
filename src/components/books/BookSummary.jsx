a6f0b584edd64431a473d4edc4e3aae2534b677a
Jina Kim <fghjiurt@naver.com>
2022-02-10 18:29:04 +0900

    Summary 분할

--- src/components/books/BookSummary.jsx
+++ src/components/books/BookSummary.jsx
@@ -3,18 +3,70 @@ import { Link } from 'react-router-dom';
 function BookSummary({ book }) {
   return (
     <div>
-      {book.photo && (
+      {book.cover_photo && (
         <img
-          src={book.photo}
+          src={book.cover_photo}
           alt={book.title}
           className="w-5 h-5 mr-1 rounded inline"
         />
       )}
       <Link to={`/books/${book.book_num}/`}>
-        {book.title} [ {book.writer} ]
+        [ {book.category_id} ] {book.title} - {book.writer}{' '}
+        {book.return_due_date}
       </Link>
     </div>
   );
 }
 
-export default BookSummary;
+function NewBookSummary({ book }) {
+  return (
+    <div>
+      {book.cover_photo && (
+        <img
+          src={book.cover_photo}
+          alt={book.title}
+          className="w-5 h-5 mr-1 rounded inline"
+        />
+      )}
+      <Link to={`/books/${book.book_num}/`}>
+        {book.title} - {book.writer}
+      </Link>
+    </div>
+  );
+}
+
+function Top5Summary({ book }) {
+  return (
+    <div>
+      {book.cover_photo && (
+        <img
+          src={book.cover_photo}
+          alt={book.title}
+          className="w-5 h-5 mr-1 rounded inline"
+        />
+      )}
+      <Link to={`/loanedbooks/${book.book_num}/`}>
+        {book.title} - {book.writer}
+      </Link>
+    </div>
+  );
+}
+
+function HeavyReaderSummary({ loanedBook }) {
+  return (
+    <div>
+      {/* {loanedBook.cover_photo && (
+        <img
+          src={loanedBook.cover_photo}
+          alt={loanedBook.title}
+          className="w-5 h-5 mr-1 rounded inline"
+        />
+      )} */}
+      <Link to={`/loanedbooks/${loanedBook.email}/`}>
+        {loanedBook.username}
+      </Link>
+    </div>
+  );
+}
+
+export { BookSummary, NewBookSummary, Top5Summary, HeavyReaderSummary };
