88bb211cefc15b73d8293a64921e85a892479d32
Jina Kim <fghjiurt@naver.com>
2022-02-10 18:34:36 +0900

    다독왕 / TOP5 초기 구성

--- src/components/main/HeavyReader.jsx
+++ src/components/main/HeavyReader.jsx
@@ -1,13 +1,14 @@
 import { useApiAxios } from 'base/api/base';
 import DebugStates from 'base/DebugStates';
 import { useAuth } from 'base/hooks/Authcontext';
+import { HeavyReaderSummary } from 'components/books/BookSummary';
 import { useEffect } from 'react';
 
 function HeavyReader() {
   const [auth] = useAuth();
-  const [{ data: _, loading, error }, refetch] = useApiAxios(
+  const [{ data: email, loading, error }, refetch] = useApiAxios(
     {
-      url: '/book/api/booklist/',
+      url: 'books/api/loanedbooks/',
       method: 'GET',
     },
     { manual: true },
@@ -20,9 +21,17 @@ function HeavyReader() {
   return (
     <div>
       <h3>♥ 다독왕 ♥</h3>
+      <div>
+        {/* {email &&
+          email
+            ?.map((email) => (
+              <HeavyReaderSummary email={email} key={email.username} />
+            ))
+            .slice(1)} */}
+      </div>
       {loading && '로딩 중 ...'}
       {error && '로딩 중 에러가 발생했습니다.'}
-      <DebugStates loading={loading} error={error} />
+      <DebugStates bookList={email} loading={loading} error={error} />
     </div>
   );
 }
--- src/components/main/Top5.jsx
+++ src/components/main/Top5.jsx
@@ -1,13 +1,14 @@
 import { useApiAxios } from 'base/api/base';
 import DebugStates from 'base/DebugStates';
 import { useAuth } from 'base/hooks/Authcontext';
+import { Top5Summary } from 'components/books/BookSummary';
 import { useEffect } from 'react';
 
 function Top5() {
   const [auth] = useAuth();
-  const [{ data: _, loading, error }, refetch] = useApiAxios(
+  const [{ data: bookList, loading, error }, refetch] = useApiAxios(
     {
-      url: '/book/api/booklist/',
+      url: '/books/api/loanedbooks/',
       method: 'GET',
     },
     { manual: true },
@@ -19,10 +20,16 @@ function Top5() {
 
   return (
     <div>
-      <h3>Top5 목록입니다.</h3>
+      <h3>Top5 목록</h3>
+      <div>
+        {bookList &&
+          bookList
+            ?.map((book) => <Top5Summary book={book} key={book.book_num} />)
+            .slice(0, 5)}
+      </div>
       {loading && '로딩 중 ...'}
       {error && '로딩 중 에러가 발생했습니다.'}
-      <DebugStates loading={loading} error={error} />
+      <DebugStates bookList={bookList} loading={loading} error={error} />
     </div>
   );
 }
