176482e51c89fb5878f87766dd3a2048b9f78e8b
Chrissy Lee <22chandl@gmail.com>
2022-02-12 20:08:17 +0900

    state 표시 뱃지 추가

--- src/components/mypage/applications/ApplicationsBooks.jsx
+++ src/components/mypage/applications/ApplicationsBooks.jsx
@@ -1,10 +1,33 @@
 import DebugStates from 'base/DebugStates';
+import Badge from 'designMaterials/Badge';
+import { useEffect, useState } from 'react';
 
 function ApplicationsBooks({ application }) {
+  const [color, setColor] = useState(() => {
+    if (application.state === 'P') {
+      return 'yellow';
+    } else if (application.state === 'D') {
+      return 'red';
+    } else {
+      return 'green';
+    }
+  });
+
+  useEffect(() => {
+    if (application.state === 'P') {
+      setColor('yellow');
+    } else if (application.state === 'D') {
+      setColor('red');
+    } else {
+      setColor('green');
+    }
+  }, [application]);
+
   return (
     <div>
       {application.title}
-      <DebugStates application={application} />
+
+      <Badge color={color}>{application.state}</Badge>
     </div>
   );
 }
