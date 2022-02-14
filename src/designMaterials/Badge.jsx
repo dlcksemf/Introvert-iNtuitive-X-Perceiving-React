b2c8b742acdcf6b29ecc6ea44d6b7be8d16544cf
Chrissy Lee <22chandl@gmail.com>
2022-02-14 11:13:54 +0900

    tailwind를 위한 색상 명시

--- src/designMaterials/Badge.jsx
+++ src/designMaterials/Badge.jsx
@@ -1,4 +1,7 @@
 function Badge({ children, color }) {
+  // bg-yellow-100 bg-red-100 bg-green-100 bg-blue-100
+  // text-yellow-800 text-red-800 text-green-800 text-blue-800
+
   return (
     <span
       className={`bg-${color}-100 text-${color}-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded h-6`}
