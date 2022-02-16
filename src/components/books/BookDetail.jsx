fc2214f606942e1783235b54a505bfba55ce72c4
Jina Kim <fghjiurt@naver.com>
2022-02-16 18:42:05 +0900

    detail css 수정

--- src/components/books/BookDetail.jsx
+++ src/components/books/BookDetail.jsx
@@ -3,7 +3,7 @@ import LoadingIndicator from 'components/LoadingIndicator';
 import LoanedModal from 'components/parts/LoanedModal';
 import LoanedIcon from 'designMaterials/LoanedIcon';
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import non_image from 'components/parts/image/non_image.jpg';
 
 function BookDetail({ book_num }) {
@@ -48,40 +48,54 @@ function BookDetail({ book_num }) {
               book_num={book?.book_num}
             />
           </div>
-          <h3>{book.title}</h3>
-          <p>{book.category_id}</p>
-          {book?.cover_photo && (
-            <img
-              src={book?.cover_photo}
-              alt={book?.title}
-              className="rounded-full m-auto"
-            />
-          )}
-          {!book?.cover_photo && (
-            <img
-              src={non_image}
-              alt="non_image"
-              className="rounded-full m-auto"
-            />
-          )}
-          <p>저자 {book.writer}</p>
-          <p>역자 {book.translator}</p>
-          <p>출판사 {book.publisher}</p>
-          <p>출판일 {book.published_date}</p>
-          <p>ISBN {book.ISBN}</p>
-          <button onClick={buyLink}>알라딘에서 구매하기</button>
-          <div className="text-center">
-            {book.story.split(/[\r\n]+/).map((line, index) => (
-              <p className="my-3" key={index}>
-                줄거리 :: {line}
-              </p>
-            ))}
+          <div className="flex justify-center">
+            <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+              {book?.cover_photo && (
+                <img
+                  src={book?.cover_photo}
+                  alt={book?.title}
+                  className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                />
+              )}
+              {!book?.cover_photo && (
+                <img
+                  src={non_image}
+                  alt="non_image"
+                  className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                />
+              )}
+              <div className="p-6 flex flex-col justify-start">
+                <h5 className="text-gray-900 text-xl font-medium mb-2">
+                  {book.title} - [{book.category_id}]
+                </h5>
+                <p className="text-gray-600 text-s">저자 {book.writer}</p>
+                <p className="text-gray-600 text-s">역자 {book.translator}</p>
+                <p className="text-gray-600 text-s">출판사 {book.publisher}</p>
+                <p className="text-gray-600 text-s">
+                  출판일 {book.published_date}
+                </p>
+                <p className="text-gray-600 text-s">ISBN {book.ISBN}</p>
+                <button onClick={buyLink} className="text-gray-600 text-s">
+                  알라딘에서 구매하기
+                </button>
+
+                <p className="text-gray-700 text-lg mb-4">
+                  {book.story.split(/[\r\n]+/).map((line, index) => (
+                    <p key={index}>{line}</p>
+                  ))}
+                </p>
+              </div>
+            </div>
           </div>
         </>
       )}
       <hr className="my-3" />
       <div className="flex gap-4 mt-3 mb-10">
-        <Link to="/books/booklist/" className="hover:text-red-400">
+        <Link
+          to="/books/booklist/"
+          type="button"
+          className="inline-block px-6 py-2 border-2 border-blue-400 text-blue-400 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+        >
           목록으로
         </Link>
       </div>
