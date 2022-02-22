9a88e89cb8009f0185d321493f215cf5330fdd7c
Chrissy Lee <22chandl@gmail.com>
2022-02-22 16:23:33 +0900

    topnav 스타일링 변경

--- src/components/parts/TopNav.jsx
+++ src/components/parts/TopNav.jsx
@@ -19,50 +19,41 @@ function TopNav({ is_main = true }) {
   };
 
   return (
-    <>
-      <div className="bg-gray-200 py-2 px-10 h-[64px] w-full">
-        <div className="flex justify-items-center justify-between items-center h-full mx-10">
-          <div>
-            {is_main ? (
-              <svg
-                style={{ width: 30 + 'px', height: 30 + 'px' }}
-                viewBox="0 0 24 24"
-                onClick={handleOpenNavigator}
-              >
-                <path
-                  className="fill-slate-600"
-                  d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"
-                />
-              </svg>
-            ) : (
-              <div className="w-10 h-full py-2" />
-            )}
-          </div>
-
-          <div>
-            <div
-              className="inline-block text-blue-500 text-bold py-2 cursor-pointer"
-              onClick={handleGoToMainPage}
+    <header class="text-gray-600 body-font sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-sm bg-opacity-20 border-b border-gray-200">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+          {is_main && (
+            <svg
+              style={{ width: 30 + 'px', height: 30 + 'px' }}
+              viewBox="0 0 24 24"
+              onClick={handleOpenNavigator}
             >
-              EUCLID SOFT
-            </div>
-          </div>
+              <path
+                className="fill-slate-600"
+                d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"
+              />
+            </svg>
+          )}
+        </div>
 
-          <div>
-            <div className="flex items-center h-full">
-              {auth.isLoggedIn && (
-                <div className="text-[16px] font-bold mr-5 py-2 inline-block">
-                  {auth.is_staff
-                    ? `WELCOME STAFF`
-                    : `WELCOME ${auth.username ? auth.username : 'NoNamer'}`}
-                </div>
-              )}
-              <PopOver />
-            </div>
+        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+          <span class="ml-3 text-xl text-primary-700">EULCIDSOFT</span>
+        </a>
+
+        <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+          <div class="inline-flex items-center border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0">
+            {auth.isLoggedIn && (
+              <div className="text-[16px] font-bold mr-5 py-2 inline-block">
+                {auth.is_staff
+                  ? `WELCOME STAFF`
+                  : `WELCOME ${auth.username ? auth.username : 'NoNamer'}`}
+              </div>
+            )}
+            <PopOver />
           </div>
         </div>
       </div>
-    </>
+    </header>
   );
 }
 
