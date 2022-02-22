14799c187b82379f579250d2597826355cb9d5fe
Jina Kim <fghjiurt@naver.com>
2022-02-22 18:43:41 +0900

    도서 목록 초기 설정 수정 중

--- src/pages/BookListPage.jsx
+++ src/pages/BookListPage.jsx
@@ -1,15 +1,8 @@
 import BookList from 'components/books/BookList';
-import Toggle from 'components/parts/Toggle';
-import { useNavigate } from 'react-router-dom';
 
 function BookListPage() {
-  const navigate = useNavigate();
-
   return (
     <div>
-      <h2 className="flex justify-center inline-block px-6 py-2 border-2 border-blue-400 text-blue-400 font-medium text-lg leading-tight uppercase rounded ease-in-out">
-        도서 목록
-      </h2>
       <BookList />
     </div>
   );
