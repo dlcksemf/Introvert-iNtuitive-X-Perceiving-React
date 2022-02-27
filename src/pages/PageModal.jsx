0d51f93366b09b0bc99c965a1d5e173022431dad
Chrissy Lee <22chandl@gmail.com>
2022-02-27 13:46:15 +0900

    모달창 스타일링 변경

--- src/pages/PageModal.jsx
+++ src/pages/PageModal.jsx
@@ -5,16 +5,9 @@ function PageModal() {
   const { modalType } = useParams();
 
   return (
-    <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-      <div className="flex flex-col justify-center h-full">
-        <div
-          id="defaultModal"
-          className="overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center h-modal md:h-full md:inset-0"
-        >
-          <div className="relative px-4 w-full max-w-2xl h-full md:h-auto">
-            <Modal modalType={modalType} />
-          </div>
-        </div>
+    <div className="bg-slate-800 bg-opacity-50 overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 flex justify-center items-center h-full md:inset-0">
+      <div className="relative px-4 h-full md:h-auto">
+        <Modal modalType={modalType} />
       </div>
     </div>
   );
