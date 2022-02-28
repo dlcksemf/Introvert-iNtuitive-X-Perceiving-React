cc2322ddcc50580bc848e110e8ed5af323a39138
kwonhaeun <wooyoung_000@naver.com>
2022-02-28 11:04:21 +0900

    관리자 도서 관리 페이지 수정

--- src/pages/Admin/PageAdminBookList.jsx
+++ src/pages/Admin/PageAdminBookList.jsx
@@ -8,13 +8,14 @@ function PageAdminBookList() {
       <div className="text-center text-white">관리자 도서 목록</div>
       <div className="-m-2 text-center mt-2">
         <div className="p-2">
-          <div className="inline-flex items-center bg-white leading-none text-black-600 rounded-full p-2 shadow text-teal text-sm">
+          <div class="inline-flex items-center bg-white leading-none text-black-600 rounded-full p-2 shadow text-teal text-sm">
             <span
-              className="inline-flex bg-blue-600 text-white rounded-full h-6 px-3 justify-center items-center cursor-pointer"
+              class="cursor-pointer inline-flex bg-indigo-600 text-white rounded-full h-6 px-3 justify-center items-center"
               onClick={() => navigate('/admin/book/new/')}
             >
-              생성
+              등록
             </span>
+            <span class="inline-flex px-2">새로운 도서를 등록해주세요!📖</span>
           </div>
         </div>
       </div>
