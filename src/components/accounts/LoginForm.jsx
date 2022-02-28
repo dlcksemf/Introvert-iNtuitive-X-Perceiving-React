df70b147dd20842219d34ddfdcb8d773bbd4ae4e
Jina Kim <fghjiurt@naver.com>
2022-02-28 10:32:43 +0900

    엔터키로 취소버튼 눌리지 않게 수정 및 로그인되게!

--- src/components/accounts/LoginForm.jsx
+++ src/components/accounts/LoginForm.jsx
@@ -70,101 +70,103 @@ function LoginForm() {
         <div className="text-red-400">로그인에 실패했습니다.</div>
       )}
 
-      <form onSubmit={handleSubmit}>
-        <div className="container px-5 py-36 mx-auto flex flex-wrap items-center">
-          <div className="lg:w-1/2 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
-            <h1
-              className="title-font font-medium text-3xl text-gray-900 text-center select-none
+      <div className="container px-5 py-36 mx-auto flex flex-wrap items-center">
+        <div className="lg:w-1/2 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+          <h1
+            className="title-font font-medium text-3xl text-gray-900 text-center select-none
             transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105"
-            >
-              📖 로그인 화면 입니다.
-            </h1>
-            <p className="leading-relaxed mt-4 text-center select-none">
-              도서를 대출 하고 싶으신가요?
-            </p>
-            <p className="leading-relaxed mt-4 text-center select-none">
-              도서를 신청 하고 싶으신가요?
-            </p>
-            <p
-              className="leading-relaxed mt-4 text-center select-none hover:text-blue-500
+          >
+            📖 로그인 화면 입니다.
+          </h1>
+          <p className="leading-relaxed mt-4 text-center select-none">
+            도서를 대출 하고 싶으신가요?
+          </p>
+          <p className="leading-relaxed mt-4 text-center select-none">
+            도서를 신청 하고 싶으신가요?
+          </p>
+          <p
+            className="leading-relaxed mt-4 text-center select-none hover:text-blue-500
             transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-100"
+          >
+            로그인 후 이용하실 수 있습니다.
+          </p>
+        </div>
+        <div className="lg:w-2/6 md:w-1/2 box-decoration-clone bg-gradient-to-r from-blue-100 to-indigo-300 rounded-lg p-8 flex flex-col md:ml-0 w-full mt-10 md:mt-0">
+          <div className="flex justify-end">
+            <div
+              className="flex justify-end"
+              onClick={() => {
+                navigate('/');
+              }}
             >
-              로그인 후 이용하실 수 있습니다.
-            </p>
-          </div>
-          <div className="lg:w-2/6 md:w-1/2 box-decoration-clone bg-gradient-to-r from-blue-100 to-indigo-300 rounded-lg p-8 flex flex-col md:ml-0 w-full mt-10 md:mt-0">
-            <div className="flex justify-end">
-              <div
-                className="flex justify-end"
-                onClick={() => {
-                  navigate('/');
-                }}
-              >
-                <CancelIcon />
-              </div>
+              <CancelIcon />
             </div>
+          </div>
+          <div>
             <h2 className="text-gray-900 text-lg font-bold title-font mb-5 select-none">
               Log In
             </h2>
-            <div className="relative mb-4">
-              <label
-                htmlFor="email"
-                className="leading-7 text-sm text-gray-600 select-none font-semibold"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                autoComplete="off"
-                value={fieldValues.email}
-                onChange={handleFieldChange}
-                placeholder="이메일 주소를 입력해주세요."
-                className="peer w-full bg-white rounded border border-gray-300 
+            <form onSubmit={handleSubmit}>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600 select-none font-semibold"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  autoComplete="off"
+                  value={fieldValues.email}
+                  onChange={handleFieldChange}
+                  placeholder="이메일 주소를 입력해주세요."
+                  className="peer w-full bg-white rounded border border-gray-300 
                 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 
                 text-base outline-none text-gray-700 py-1 px-3 leading-8 
                 transition-colors duration-200 ease-in-out hover:font-bold"
-              />
-              <p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-                올바른 이메일 형식을 지켜주세요.
-              </p>
-            </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="password"
-                className="leading-7 text-sm text-gray-600 select-none font-semibold"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                autoComplete="off"
-                value={fieldValues.password}
-                onChange={handleFieldChange}
-                placeholder="비밀번호를 입력해주세요."
-                className="peer w-full bg-white rounded border border-gray-300 hover:font-bold focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-              <p className="mb-2 mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-                비밀번호는 8자리 이상 입력해주세요.
-              </p>
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-indigo-600 border-0 py-3 px-8 
+                />
+                <p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                  올바른 이메일 형식을 지켜주세요.
+                </p>
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="password"
+                  className="leading-7 text-sm text-gray-600 select-none font-semibold"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  autoComplete="off"
+                  value={fieldValues.password}
+                  onChange={handleFieldChange}
+                  placeholder="비밀번호를 입력해주세요."
+                  className="peer w-full bg-white rounded border border-gray-300 hover:font-bold focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <p className="mb-2 mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                  비밀번호는 8자리 이상 입력해주세요.
+                </p>
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-indigo-600 border-0 py-3 px-8 w-full 
               focus:outline-none hover:bg-indigo-600 rounded text-lg hover:bg-indigo-700
               transition duration-500 ease-in-out hover:scale-105 rounded-full"
-            >
-              로그인
-            </button>
+              >
+                로그인
+              </button>
+            </form>
             <p className="text-xs text-gray-500 mt-8 select-none">
               (주) EUCLID SOFT
             </p>
           </div>
         </div>
-      </form>
+      </div>
       <ToastContainer />
     </section>
   );
