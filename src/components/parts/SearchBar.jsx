0cc2842dcb38fe48dd9a55f73a85d1c02ad9552c
kangaerin <rosyty2@naver.com>
2022-03-02 17:07:30 +0900

    한글로 수정중

--- src/components/parts/SearchBar.jsx
+++ src/components/parts/SearchBar.jsx
@@ -12,10 +12,10 @@ function SearchBar({ handleSubmit, handleChange }) {
       <form onSubmit={handleSubmitButtonClicked}>
         <input
           className="w-[200px] inline-block border-2 border-gray-300 bg-white h-10 px-5
-          pr-16 rounded-lg text-sm focus:outline-none"
+          pr-8 rounded-lg text-sm focus:outline-none"
           type="search"
           name="search"
-          placeholder="search"
+          placeholder="검색어를 입력해주세요."
           onChange={(e) => {
             handleChange(e.target.value);
           }}
