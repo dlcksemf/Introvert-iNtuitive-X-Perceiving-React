849ecc910cce5c6c469d9362252bb643d2685e77
Jina Kim <fghjiurt@naver.com>
2022-03-03 00:02:25 +0900

    footer 수정

--- src/components/parts/Footer.jsx
+++ src/components/parts/Footer.jsx
@@ -1,4 +1,4 @@
-import hope_book from './image/hope_book.jpg';
+import hopeBook from './image/hopeBook.png';
 import { useLocation } from 'react-router-dom';
 
 function Footer() {
@@ -6,42 +6,14 @@ function Footer() {
 
   return (
     <header
-      className="bg-gray-200 text-gray-600 body-font sticky top-0 bg-white pt-3 pb-10"
+      className="bg-stone-100 text-gray-600 body-font sticky top-0 bg-white pt-3 pb-5"
       style={{ zIndex: location.pathname === '/' ? 900 : 0 }}
     >
       <div className="flex">
-        <div className="inline-block text-semibold select-none ml-10 mt-3">
-          (주)유클리드소프트 - 대전 동구 계족로 151
+        <div className="inline-block text-semibold text-sm select-none ml-20 mt-2 tracking-widest">
+          ㈜ 유클리드 소프트 | 대전 동구 계족로 151 601호 | ☎ 042) 488-6589 | ✉
+          euclidsoft.lab@gmail.com
         </div>
-
-        <div className="absolute right-40 inline-block mt-2">
-          <div className="">
-            <svg
-              className="cursor-pointer"
-              onClick={() =>
-                window.open('https://www.facebook.com/euclidsoft.lab/')
-              }
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 48 48"
-              width="48px"
-              height="48px"
-            >
-              <path
-                fill="#039be5"
-                d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"
-              />
-              <path
-                fill="#fff"
-                d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
-              />
-            </svg>
-          </div>
-        </div>
-        <div
-          className="absolute right-10 ml-1 mt-3 w-28 h-10 bg-contain cursor-pointer inline-block"
-          style={{ backgroundImage: `url(${hope_book})` }}
-          onClick={() => window.open('http://djbook.or.kr/', '_blank')}
-        ></div>
       </div>
     </header>
   );
