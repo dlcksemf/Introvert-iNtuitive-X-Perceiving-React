1a678c011199d4df3fe0c7f986e0b82c49361e43
Jina Kim <fghjiurt@naver.com>
2022-03-02 14:55:55 +0900

    메인 꿈돌이 캐릭터 / 파비콘 변경

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -22,7 +22,7 @@ function Main() {
           <img
             src={ggumdori}
             alt="꿈돌이"
-            className="mt-64 ml-8 object-contain h-48 w-96"
+            className="mt-64 ml-8 object-contain h-48 w-96 stroke-2 stroke-black"
           />
           <div
             className="text-white bg-indigo-600 border-0 py-4 ml-28 mr-24
