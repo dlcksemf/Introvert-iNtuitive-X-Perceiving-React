4bd03ab3341cd33302986063a23c5be144dc1d40
Jina Kim <fghjiurt@naver.com>
2022-03-03 01:21:09 +0900

    메인화면 꿈돌이 이동

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,33 +6,35 @@ function Main() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <span className="flex justify-center">
+    <>
+      <span className="relative flex justify-center">
         <img
           src={book}
           alt="메인 책장"
           className="w-screen h-screen rounded inline opacity-95"
         />
-        <button
-          onClick={() => {
-            navigate(`/books/bookList/`);
-          }}
-          className="absolute transition duration-500 ease-in-out hover:-translate-y-6 hover:scale-100"
-        >
-          <img
-            src={ggumdori}
-            alt="꿈돌이"
-            className="mt-64 ml-8 object-contain h-48 w-96 stroke-2 stroke-black"
-          />
-          <div
-            className="text-white bg-indigo-600 border-0 py-4 ml-28 mr-24
-              focus:outline-none hover:bg-indigo-700 rounded-full text-lg"
+        <div className="absolute right-24">
+          <button
+            onClick={() => {
+              navigate(`/books/bookList/`);
+            }}
+            className="mt-20 transition duration-500 ease-in-out hover:-translate-y-6 hover:scale-100"
           >
-            도서 목록 보러 가기
-          </div>
-        </button>
+            <img
+              src={ggumdori}
+              alt="꿈돌이"
+              className="mt-36 ml-4 object-contain h-48 w-96 stroke-2 stroke-black"
+            />
+            <h1
+              className="text-white bg-indigo-600 border-0 py-3 ml-24 mr-20
+              focus:outline-none hover:bg-indigo-700 rounded-full text-lg"
+            >
+              도서 목록 보러 가기
+            </h1>
+          </button>
+        </div>
       </span>
-    </div>
+    </>
   );
 }
 export default Main;
