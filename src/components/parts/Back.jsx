709063dee8bde14d093efda5f5a12754a3af8894
Jina Kim <fghjiurt@naver.com>
2022-03-03 18:53:57 +0900

    뒤로가기/앞으로 가기 버튼 구현중...

---
+++ src/components/parts/Back.jsx
@@ -0,0 +1,15 @@
+// import Button from 'designMaterials/Button';
+// import { useNavigate } from 'react-router-dom';
+// import left from 'components/parts/image/left.png';
+// import right from 'components/parts/image/right.png';
+// import click from 'components/parts/image/click.png';
+// import { useState } from 'react';
+
+function Back() {
+  //   const navigate = useNavigate();
+  //   const [open, setOpen] = useState(false);
+
+  return <div className="absolute left-full">dd</div>;
+}
+
+export default Back;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,34 +2,20 @@ import HeavyReader from 'components/main/HeavyReader';
 import Main from 'components/main/Main';
 import NewBook from 'components/main/NewBook';
 import Top5 from 'components/main/Top5';
+import Back from 'components/parts/Back';
 import MainNavigation from 'components/parts/MainNavigation';
 import GuidePage from './GuidePage';
-import left from 'components/parts/image/left.png';
-import right from 'components/parts/image/right.png';
-import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
-  const navigate = useNavigate();
-
   return (
     <div>
       <MainNavigation />
 
+      <div className="sticky top-3/4 z-50">
+        <Back />
+      </div>
+
       <div>
-        <div className="sticky top-full flex justify-end">
-          <button
-            className="w-14 h-14 cursor-pointer"
-            onClick={() => navigate(-1)}
-          >
-            <img src={left} alt="뒤로가기" />
-          </button>
-          <button
-            className="ml-5 w-14 h-14 cursor-pointer"
-            onClick={() => navigate(1)}
-          >
-            <img src={right} alt="앞으로가기" />
-          </button>
-        </div>
         <div className="ml-[80px] z-0">
           <section id="main">
             <Main />
