6a112e6fe8beb00e76f95e296f1b98a8f699f360
Jina Kim <fghjiurt@naver.com>
2022-03-03 21:19:49 +0900

    floating button ing...

--- src/components/parts/Back.jsx
+++ src/components/parts/Back.jsx
@@ -1,15 +1,54 @@
-// import Button from 'designMaterials/Button';
-// import { useNavigate } from 'react-router-dom';
-// import left from 'components/parts/image/left.png';
-// import right from 'components/parts/image/right.png';
-// import click from 'components/parts/image/click.png';
-// import { useState } from 'react';
+import {
+  FloatingMenu,
+  MainButton,
+  ChildButton,
+  Directions,
+} from 'react-floating-button-menu';
+import { useState } from 'react';
+import Button from 'designMaterials/Button';
 
 function Back() {
   //   const navigate = useNavigate();
-  //   const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  return <div className="absolute left-full">dd</div>;
+  return (
+    <>
+      <div className="relative left-3/4">
+        <FloatingMenu
+          slideSpeed={500}
+          direction={Directions.Up}
+          spacing={8}
+          isOpen={open}
+        >
+          <MainButton
+            iconResting={
+              <Button style={{ fontSize: 20 }} nativeColor="white" />
+            }
+            iconActive={<Button style={{ fontSize: 20 }} nativeColor="white" />}
+            background="black"
+            onClick={() => setOpen(true)}
+            size={56}
+          />
+          <ChildButton
+            icon={<Button style={{ fontSize: 20 }} />}
+            background="white"
+            size={40}
+            onClick={() => console.log('First button clicked')}
+          />
+          <ChildButton
+            icon={<Button style={{ fontSize: 20 }} />}
+            background="white"
+            size={40}
+          />
+          <ChildButton
+            icon={<Button style={{ fontSize: 20 }} />}
+            background="white"
+            size={40}
+          />
+        </FloatingMenu>
+      </div>
+    </>
+  );
 }
 
 export default Back;
