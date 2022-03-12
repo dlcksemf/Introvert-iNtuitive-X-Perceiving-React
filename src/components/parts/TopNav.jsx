636989ed994234140b0800e3c75704bb501110ef
Jina Kim <fghjiurt@naver.com>
2022-03-13 03:30:47 +0900

    topnav 아이콘 설정 변경

--- src/components/parts/TopNav.jsx
+++ src/components/parts/TopNav.jsx
@@ -3,7 +3,8 @@
 import { useAuth } from 'base/hooks/Authcontext';
 import { NavLink, useNavigate } from 'react-router-dom';
 import name from 'components/parts/image/name2.png';
-import search from 'components/parts/image/search3.png';
+import search from 'components/parts/image/search.png';
+import user from 'components/parts/image/user.png';
 
 function TopNav() {
   const navigate = useNavigate();
@@ -60,23 +61,14 @@ function TopNav() {
                   navigate('/accounts/mypage/');
                 }}
               >
-                <svg
-                  style={{ width: 30 + 'px', height: 30 + 'px' }}
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    className="fill-slate-600 justify-end"
-                    d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"
-                  />
-                </svg>
+                <img src={user} alt="내정보" className="h-8 w-8" />
               </button>
             </div>
           )}
           <img
             src={search}
             alt="검색"
-            style={{ width: 25 + 'px', height: 25 + 'px' }}
-            className="flex justify-end transition duration-500 ease-in-out hover:scale-125"
+            className="h-9 w-9 flex justify-end transition duration-500 ease-in-out hover:scale-125"
           />
         </div>
       </div>
