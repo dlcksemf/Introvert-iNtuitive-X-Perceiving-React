de3e2c52b6972beaa93ef2aea48b4255010f3b94
Jina Kim <fghjiurt@naver.com>
2022-03-14 10:19:02 +0900

    메인페이지 도서목록 링크 연결 및 CSS추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,16 +2,51 @@ import MainNavigation from 'components/parts/MainNavigation';
 import Top5 from 'components/main/Top5';
 import HeavyReader from 'components/main/HeavyReader';
 import NewBook from 'components/main/NewBook';
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <MainNavigation />
       <header class="grid grid-cols-8 text-center">
-        <div className="col-start-3 select-none">도서목록</div>
-        <div className="col-start-4 select-none">도서신청</div>
-        <div className="col-start-5 select-none">보드게임</div>
-        <div className="col-start-6 select-none">이용안내</div>
+        <div
+          className="col-start-3 select-none transition duration-500 ease-in-out hover:scale-125 cursor-point
+          hover:text-blue-700 hover:font-extrabold"
+          onClick={() => {
+            navigate(`/books/booklist/`);
+          }}
+        >
+          도서목록
+        </div>
+        <div
+          className="col-start-4 select-none transition duration-500 ease-in-out hover:scale-125 cursor-point
+          hover:text-blue-700 hover:font-extrabold"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        >
+          도서신청
+        </div>
+        <div
+          className="col-start-5 select-none transition duration-500 ease-in-out hover:scale-125 cursor-point
+          hover:text-blue-700 hover:font-extrabold"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        >
+          보드게임
+        </div>
+        <div
+          className="col-start-6 select-none transition duration-500 ease-in-out hover:scale-125 cursor-point
+          hover:text-blue-700 hover:font-extrabold"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        >
+          이용안내
+        </div>
       </header>
       <div class="grid grid-rows-4 grid-flow-col gap-4 text-center">
         <div class="row-span-4">
