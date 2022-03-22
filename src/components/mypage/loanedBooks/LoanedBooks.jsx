cd5707840967007932b70c12f3a05711c113e657
kwonhaeun <wooyoung_000@naver.com>
2022-03-22 10:58:17 +0900

    도서 반납 return 모달

--- src/components/mypage/loanedBooks/LoanedBooks.jsx
+++ src/components/mypage/loanedBooks/LoanedBooks.jsx
@@ -2,6 +2,7 @@ import { useApiAxios } from 'base/api/base';
 import { useAuth } from 'base/hooks/Authcontext';
 import { useReload } from 'base/hooks/ReloadContext';
 import Badge from 'designMaterials/Badge';
+import PageReturnModal from 'pages/PageReturnModal';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ function LoanedBooks({ book }) {
   const [auth] = useAuth();
   const [, setReload] = useReload();
   const navigate = useNavigate();
+  const [showReturn, setShowReturn] = useState(false);
 
   let today = new Date();
   const date =
@@ -41,33 +43,33 @@ function LoanedBooks({ book }) {
     { manual: true },
   );
 
-  const handleClickSubmitButton = (e) => {
-    e.preventDefault();
-    if (window.confirm('반납하시겠습니까?')) {
-      handleOkButton();
-      alert('반납 되었습니다.');
-    } else {
-      handleCancleButton();
-      alert('취소되었습니다.');
-    }
-    setBookReturn(true);
-  };
+  // const handleClickSubmitButton = (e) => {
+  //   e.preventDefault();
+  //   if (window.confirm('반납하시겠습니까?')) {
+  //     handleOkButton();
+  //     alert('반납 되었습니다.');
+  //   } else {
+  //     handleCancleButton();
+  //     alert('취소되었습니다.');
+  //   }
+  //   setBookReturn(true);
+  // };
 
-  const handleOkButton = () => {
-    updateState({ data: { return_state: 'R' } })
-      .then(() => {
-        setBookReturn(false);
-        setReload(true);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    window.location.replace('/accounts/mypage/');
-  };
+  // const handleOkButton = () => {
+  //   updateState({ data: { return_state: 'R' } })
+  //     .then(() => {
+  //       setBookReturn(false);
+  //       setReload(true);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   window.location.replace('/accounts/mypage/');
+  // };
 
-  const handleCancleButton = () => {
-    setBookReturn(false);
-  };
+  // const handleCancleButton = () => {
+  //   setBookReturn(false);
+  // };
 
   return (
     <React.Fragment>
@@ -101,7 +103,14 @@ function LoanedBooks({ book }) {
 
         <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
           {book.return_state === 'L' && (
-            <button onClick={handleClickSubmitButton}>반납 신청</button>
+            <button onClick={() => setShowReturn(true)}>반납 신청</button>
+          )}
+
+          {showReturn && (
+            <PageReturnModal
+              updateState={updateState}
+              handleClose={() => setShowReturn(false)}
+            />
           )}
         </td>
       </tr>
