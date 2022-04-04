c2ee1b6c185119be0d193fd24e133fdaeee51392
kangaerin <rosyty2@naver.com>
2022-04-04 16:58:10 +0900

    디테일 에러 해결

--- src/components/Game/GameSummary.jsx
+++ src/components/Game/GameSummary.jsx
@@ -11,28 +11,22 @@ function GameSummary({ game }) {
     <div className="m-auto px-4 py-4 max-w-xl">
       <div className="bg-gray-100 hover:bg-blue-100 border-gray-100 border-2 rounded-lg overflow-hidden mb-10">
         {game.game_cover_photo && (
-          <Link to={`/game/${game.game_num.result}/`}>
+          <Link to={`/game/${game.game_num}/`}>
             <img
               src={game.game_cover_photo}
-              alt={game.game_name.result}
+              alt={game.game_name}
               className="w-full"
             />
           </Link>
         )}
         {!game?.game_cover_photo && (
-          <Link to={`/game/${game.game_num.result}/`}>
-            <img
-              src={non_image}
-              alt={game.game_name.result}
-              className="w-full"
-            />
+          <Link to={`/game/${game.game_num}/`}>
+            <img src={non_image} alt={game.game_name} className="w-full" />
           </Link>
         )}
         <div className="p-8 sm:p-9 md:p-7 xl:p-9">
           <h3 className="font-semibold text-dark text-center">
-            <Link to={`/game/${game.game_num.result}/`}>
-              {game.game_name.result}
-            </Link>
+            <Link to={`/game/${game.game_num}/`}>{game.game_name}</Link>
           </h3>
         </div>
       </div>
