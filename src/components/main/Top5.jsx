4225240489aaa11e54f495916c8e3ab15979975f
Jina Kim <fghjiurt@naver.com>
2022-04-12 14:26:31 +0900

    인기도서 수정완

--- src/components/main/Top5.jsx
+++ src/components/main/Top5.jsx
@@ -19,35 +19,26 @@ function Top5() {
   }, [auth, refetch]);
 
   return (
-    <div className="ml-32">
-      <h1 className="text-4xl font-semibold relative ml-40 top-20 text-gray-700">
-        인기도서
-      </h1>
-
-      <div className="">
-        <div className="h-[650px] w-[750px]">
-          <Carousel
-            stopAutoPlayOnHover={true}
-            fullHeightHover={false}
-            indicators={false}
-            NavButton={false}
-            navButtonsAlwaysInvisible={true}
-            swipeable={true}
-            swipeScrollTolerance={10}
-          >
-            {bookList
-              ?.sort((book1, book2) => book2.count_loans - book1.count_loans)
-              .slice(0, 5)
-              .map((book) => (
-                <div
-                  key={book.book_num}
-                  className="ml-32 h-[600px] w-[650px] flex justify-center items-center"
-                >
-                  <Top5Summary book={book} />
-                </div>
-              ))}
-          </Carousel>
-        </div>
+    <div>
+      <div>
+        <Carousel
+          stopAutoPlayOnHover={true}
+          fullHeightHover={false}
+          indicators={false}
+          NavButton={false}
+          navButtonsAlwaysInvisible={true}
+          swipeable={true}
+          swipeScrollTolerance={10}
+        >
+          {bookList
+            ?.sort((book1, book2) => book2.count_loans - book1.count_loans)
+            .slice(0, 5)
+            .map((book) => (
+              <div key={book.book_num}>
+                <Top5Summary book={book} />
+              </div>
+            ))}
+        </Carousel>
       </div>
     </div>
   );
