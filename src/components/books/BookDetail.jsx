f8d801da0162dc7cb0092fdf01588a7ebb32f836
Jina Kim <fghjiurt@naver.com>
2022-04-13 05:22:31 +0900

    도서 상세 페이지 수정중

--- src/components/books/BookDetail.jsx
+++ src/components/books/BookDetail.jsx
@@ -70,87 +70,121 @@ function BookDetail({ book_num }) {
       {error && navigate(`*`)}
       {book && (
         <>
-          <section className="text-gray-600 body-font overflow-hidden">
-            <div className="container px-5 py-24 mx-auto">
-              <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                {book?.cover_photo && (
-                  <img
-                    src={book?.cover_photo}
-                    alt={book?.title}
-                    className="lg:w-2/6 w-full lg:h-2/6 h-64 object-cover object-center ml-28 mr-10 mt-14"
-                  />
+          <div className="border-b-4 border-sky-600 w-3/4 relative left-[200px]">
+            <h2 className="text-3xl font-bold relative bottom-[20px] left-[20px] select-none">
+              도서 상세 정보
+            </h2>
+          </div>
+          <div className="relative top-[40px] left-[325px] w-[200px] select-none shadow-2xl">
+            {book?.cover_photo && (
+              <img
+                src={book?.cover_photo}
+                alt={book?.title}
+                className="lg:w-[200px] w-full lg:h-2/6 h-64 object-cover object-center"
+              />
+            )}
+            {!book?.cover_photo && (
+              <img
+                src={non_image}
+                alt="non_image"
+                className="lg:w-[200px] w-full lg:h-2/6 h-64 object-cover object-center"
+              />
+            )}
+          </div>
+          <section
+            className="text-left text-gray-600 select-none
+          relative left-[580px] bottom-[220px] w-[890px]"
+          >
+            {book?.category && (
+              <h2 className="mb-2">카테고리 : [ {book?.category} ]</h2>
+            )}
+            <h1 className="mb-2">제 목 : {book?.title}</h1>
+            <h1 className="mb-2">작 가 : {book?.writer}</h1>
+            {book?.translator && (
+              <h1 className="mb-2">역 자 : {book?.translator}</h1>
+            )}
+            <h1 className="mb-2">출판사 : {book?.publisher}</h1>
+            <h1 className="mb-2">출판일 : {book?.published_date}</h1>
+            <h1 className="mb-2">ISBN : {book?.ISBN}</h1>
+            <h1 className="mb-2">수 량 : {book?.amount} 권</h1>
+            <button
+              onClick={buyLink}
+              className="text-indigo-900 text-s hover:font-bold
+              relative left-[300px] bottom-[270px]"
+            >
+              @알라딘에서 책찾기
+            </button>
+            <div className="flex">
+              <span className="text-gray-600 m-auto select-none">찜하기</span>
+              <div className="mt-2 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110">
+                <Toggle
+                  book={book}
+                  wish={wish?.results[0]}
+                  user_id={auth.user_id}
+                  getWish={getWish}
+                  reload={reload}
+                />
+              </div>
+              <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                {book?.state === 'A' && (
+                  <>
+                    <span className="text-gray-600 m-auto select-none">
+                      대출하기
+                    </span>
+                    <div onClick={handleClickLoan} className="">
+                      <LoanedIcon />
+                    </div>
+                  </>
                 )}
-                {!book?.cover_photo && (
-                  <img
-                    src={non_image}
-                    alt="non_image"
-                    className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                  />
+
+                {book?.state !== 'A' && (
+                  <p className="m-auto select-none hover:text-indigo-600">
+                    반납 예정일 :: {''}
+                    {book?.loaned_books[0]?.return_due_date}
+                  </p>
                 )}
-                <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                  {book?.category && (
-                    <h2 className="text-sm title-font text-gray-500 tracking-widest select-none mt-5 mb-3">
-                      [ {book?.category} ]
-                    </h2>
-                  )}
-                  <h1 className="text-gray-900 text-3xl title-font font-bold mb-5 select-none">
-                    {book?.title}
-                  </h1>
-                  <h1 className="text-gray-900 text-xl title-font font-medium mb-5 select-none">
-                    {book?.writer}
-                  </h1>
-                  <div className="flex mb-4 select-none">
-                    {book?.translator && (
-                      <>
-                        <span className="flex items-center select-none">
-                          역자 {book?.translator}
-                        </span>
-                        <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s" />
-                      </>
-                    )}
-                    <span className="flex py-2 select-none">
-                      {book?.publisher}
-                    </span>
-                    <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s select-none">
-                      {book?.published_date}
-                    </span>
-                    <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s select-none">
-                      ISBN {book?.ISBN}
-                    </span>
-                    <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s select-none">
-                      {book?.amount} 권
-                    </span>
-                  </div>
-                  <div className="leading-relaxed select-none mt-14 hover:text-gray-900">
-                    {book?.story.split(/[\r\n]+/).map((line, index) => (
-                      <p key={index}>{line}</p>
-                    ))}
-                  </div>
-                  <div className="flex mt-10 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                    <button
-                      onClick={buyLink}
-                      className="text-indigo-900 text-s mb-20 hover:font-bold"
-                    >
-                      @알라딘에서 책찾기
-                    </button>
-                  </div>
-                  <div className="flex justify-between">
-                    <Link
-                      to={
-                        state?.beforeLocation
-                          ? `/books/booklist/${state.beforeLocation}`
-                          : `/books/booklist/`
-                      }
-                      state={{ pathname: pathname }}
-                    >
-                      <div
-                        className="flex m-auto ml-auto 
+                <LoanedModal
+                  ariaHideApp={false}
+                  modalIsOpen={modalIsOpen}
+                  setModalIsOpen={setModalIsOpen}
+                  book_num={book?.book_num}
+                  reload={reload}
+                />
+              </span>
+            </div>
+          </section>
+          <div className="leading-relaxed w-[850px] select-none relative left-[300px] bottom-[120px]">
+            <h1 className="text-2xl font-bold relative bottom-[10px]">
+              책소개
+            </h1>
+            {book?.story.split(/[\r\n]+/).map((line, index) => (
+              <p key={index}>{line}</p>
+            ))}
+          </div>
+          <Link
+            to={
+              state?.beforeLocation
+                ? `/books/booklist/${state.beforeLocation}`
+                : `/books/booklist/`
+            }
+            state={{ pathname: pathname }}
+          >
+            <div
+              className="flex m-auto ml-auto 
                   text-gray-600 hover:text-indigo-600 
                   border-2 border-gray-200 py-2 px-6 focus:outline-none rounded"
-                      >
-                        목록으로
-                      </div>
-                    </Link>
+            >
+              목록으로
+            </div>
+          </Link>
+          <div className="flex justify-center select-none">
+            <h1 className="text-2xl font-bold">한줄평</h1>
+            <div>
+              <ReviewPage book={book} reload={setReloading} />
+            </div>
+          </div>
+          {/* 
+               
                     <div className="flex">
                       <span className="text-gray-600 m-auto select-none">
                         찜하기
@@ -194,13 +228,8 @@ function BookDetail({ book_num }) {
                   </div>
                 </div>
               </div>
-              <div className="flex justify-center">
-                <div>
-                  <ReviewPage book={book} reload={setReloading} />
-                </div>
-              </div>
             </div>
-          </section>
+          </section> */}
         </>
       )}
       <ToastContainer />
