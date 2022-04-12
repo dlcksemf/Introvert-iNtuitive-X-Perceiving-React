71877f71630dd7895bcfd44cb55933d7e982955b
Jina Kim <fghjiurt@naver.com>
2022-04-12 10:06:15 +0900

    네비게이션 디자인 확정

--- src/components/parts/SecondNav.jsx
+++ src/components/parts/SecondNav.jsx
@@ -1,8 +1,6 @@
 import { useAuth } from 'base/hooks/Authcontext';
 import { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import name from 'components/parts/image/euclidLibrary.png';
-import user from 'components/parts/image/user.png';
 import Logo from 'components/parts/image/Logo.png';
 
 const useScroll = () => {
@@ -39,112 +37,19 @@ function SecondNav() {
 
   if (y > 0.5) {
     return (
-      // <div className="fixed w-full sm:body-font top-0 z-10">
-      //   <header className="text-gray-700 bg-white backdrop-filter backdrop-blur-sm bg-opacity-90">
-      //     <div className="flex justify-between">
-      //       <div>
-      //         <img
-      //           src={name}
-      //           alt="EUCLID BOOKS"
-      //           className="h-20 cursor-pointer"
-      //           onClick={() => {
-      //             navigate(`/`);
-      //           }}
-      //         />
-      //       </div>
-      //       <div>
-      //         <header className="ml-52 mt-5 grid grid-cols-4 text-xl">
-      //           <div
-      //             className="mr-32 select-none transition duration-500 ease-in-out cursor-pointer
-      //     hover:text-indigo-700"
-      //             onClick={() => {
-      //               navigate(`/books/booklist/`);
-      //             }}
-      //           >
-      //             도서목록
-      //           </div>
-      //           <div
-      //             className=" select-none transition duration-500 ease-in-out cursor-pointer
-      //     hover:text-indigo-700 "
-      //             onClick={() => {
-      //               navigate(`/books/application/`);
-      //             }}
-      //           >
-      //             신청도서
-      //           </div>
-      //           <div
-      //             className=" select-none transition duration-500 ease-in-out cursor-pointer
-      //     hover:text-indigo-700 "
-      //             onClick={() => {
-      //               navigate(`/game/gamelist/`);
-      //             }}
-      //           >
-      //             보드게임
-      //           </div>
-      //           <div
-      //             className=" select-none transition duration-500 ease-in-out cursor-pointer
-      //     hover:text-indigo-700 "
-      //             onClick={() => {
-      //               navigate(`/guidepage/`);
-      //             }}
-      //           >
-      //             이용안내
-      //           </div>
-      //         </header>
-      //       </div>
-      //       <div>
-      //         <div
-      //           className="grow-0 shrink-0 flex justify-end items-center basis-1/3 border-0 py-1 pt-5
-      //         pl-3 focus:outline-none rounded text-base mt-8 md:mt-0"
-      //         >
-      //           {!auth.isLoggedIn && (
-      //             <NavLink
-      //               className="font-semibold select-none mr-3 transition duration-500 ease-in-out
-      //               hover:-translate-y-1 hover:scale-110"
-      //               type="button"
-      //               to="/accounts/login/"
-      //             >
-      //               로그인
-      //             </NavLink>
-      //           )}
-      //           {auth.isLoggedIn && (
-      //             <div className="text-sm font-bold mr-14 select-none flex">
-      //               <div className="mt-1">
-      //                 {auth.is_staff
-      //                   ? `관리자님 환영합니다`
-      //                   : `${
-      //                       auth.username ? auth.username : 'NoNamer'
-      //                     }님 환영합니다`}
-      //               </div>
-      //               <button
-      //                 className="ml-3 transition duration-500 ease-in-out hover:scale-125"
-      //                 data-bs-toggle="tooltip"
-      //                 data-bs-placement="top"
-      //                 title="내정보 가기"
-      //                 onClick={() => {
-      //                   navigate('/accounts/mypage/');
-      //                 }}
-      //               >
-      //                 <img src={user} alt="내정보" className="h-8 w-8" />
-      //               </button>
-      //             </div>
-      //           )}
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </header>
-      // </div>
       <div className="fixed w-full body-font top-0 z-10 h-68">
         <div class="flex-1 flex flex-col">
           <nav class="px-10 flex justify-between bg-white h-16 shadow-md">
-            {/* <!-- top bar left --> */}
             <ul class="flex items-center">
-              {/* <!-- add button --> */}
-              <img src={Logo} alt="EUCLID BOOKS" className="h-[50px]" />
+              <div
+                className="text-md text-primary-600 text-bold text-center cursor-pointer"
+                onClick={handleGoToMainPage}
+              >
+                <img src={Logo} alt="EUCLID BOOKS" className="h-[50px]" />
+              </div>
             </ul>
 
             <ul class="flex items-center">
-              {/* <!-- add button --> */}
               <li>
                 <header className="grid grid-cols-8 text-center text-xl mt-6">
                   <div
@@ -187,7 +92,6 @@ function SecondNav() {
               </li>
             </ul>
 
-            {/* <!-- to bar right  --> */}
             <ul class="flex items-center">
               <li>
                 {!auth.isLoggedIn && (
@@ -250,110 +154,10 @@ function SecondNav() {
   } else {
     return (
       <>
-        {/* <div className="fixed w-full body-font top-0 z-10 h-68">
-          <header className="text-gray-700 bg-white backdrop-filter backdrop-blur-sm bg-opacity-90">
-            <div className="mx-2 flex px-7 pt-1 pb-4 flex-col md:flex-row items-center">
-              <div className="basis-1/3 grow-0 shrink-0 text-gray-900 md:mb-0"></div>
-
-              <div
-                className={`flex justify-center basis-1/3 grow-0 shrink-0 md:ml-auto md:mr-auto ${
-                  auth.is_staff ? 'cursor-default' : 'cursor-pointer'
-                }`}
-              >
-                <div
-                  className="text-md text-primary-600 text-bold text-center"
-                  onClick={handleGoToMainPage}
-                >
-                  <img src={name} alt="EUCLID BOOKS" className="h-32" />
-                </div>
-              </div>
-
-              <div
-                className="grow-0 shrink-0 flex justify-end items-center basis-1/3 border-0 
-            py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0"
-              >
-                {!auth.isLoggedIn && (
-                  <NavLink
-                    className="font-semibold select-none mr-6 transition duration-500 ease-in-out 
-                  hover:-translate-y-1 hover:scale-110"
-                    type="button"
-                    to="/accounts/login/"
-                  >
-                    로그인
-                  </NavLink>
-                )}
-                {auth.isLoggedIn && (
-                  <div className="text-sm font-bold mr-3 select-none flex">
-                    <div className="mt-1">
-                      {auth.is_staff
-                        ? `관리자님 환영합니다`
-                        : `${
-                            auth.username ? auth.username : 'NoNamer'
-                          }님 환영합니다`}
-                    </div>
-                    <button
-                      className="ml-3 transition duration-500 ease-in-out hover:scale-125"
-                      data-bs-toggle="tooltip"
-                      data-bs-placement="top"
-                      title="내정보 가기"
-                      onClick={() => {
-                        navigate('/accounts/mypage/');
-                      }}
-                    >
-                      <img src={user} alt="내정보" className="h-8 w-8" />
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
-            <div>
-              <header className=" grid grid-cols-8 text-center text-xl">
-                <div
-                  className="col-start-3 select-none transition duration-500 ease-in-out cursor-pointer
-          hover:text-indigo-700 mb-5"
-                  onClick={() => {
-                    navigate(`/books/booklist/`);
-                  }}
-                >
-                  도서목록
-                </div>
-                <div
-                  className="col-start-4 select-none transition duration-500 ease-in-out cursor-pointer
-          hover:text-indigo-700"
-                  onClick={() => {
-                    navigate(`/books/application/`);
-                  }}
-                >
-                  신청도서
-                </div>
-                <div
-                  className="col-start-5 select-none transition duration-500 ease-in-out cursor-pointer
-          hover:text-indigo-700"
-                  onClick={() => {
-                    navigate(`/game/gamelist/`);
-                  }}
-                >
-                  보드게임
-                </div>
-                <div
-                  className="col-start-6 select-none transition duration-500 ease-in-out cursor-pointer
-          hover:text-indigo-700"
-                  onClick={() => {
-                    navigate(`/guidepage/`);
-                  }}
-                >
-                  이용안내
-                </div>
-              </header>
-            </div>
-          </header>
-        </div> */}
         <div className="fixed w-full body-font top-0 z-10 h-68">
           <div class="flex-1 flex flex-col">
             <nav class="px-10 flex justify-between h-[55px] border-b-2 bg-sky-600">
-              {/* <!-- top bar left --> */}
               <ul class="flex items-center">
-                {/* <!-- add button --> */}
                 <li class="relative top-[58px] w-[250px]">
                   <div
                     className={`flex justify-center basis-1/3 grow-0 shrink-0 md:ml-auto md:mr-auto ${
@@ -370,7 +174,6 @@ function SecondNav() {
                 </li>
               </ul>
 
-              {/* <!-- to bar right  --> */}
               <ul class="flex items-center">
                 <li>
                   {!auth.isLoggedIn && (
