84a151ba469a89129fd7eec9cc4ee994b95d2115
Yerin Park <1472moment@gmail.com>
2022-04-15 13:47:09 +0900

    AdminGameListPage.jsx, PageAdminBookList.jsx // border 추가

--- src/pages/Admin/Game/AdminGameListPage.jsx
+++ src/pages/Admin/Game/AdminGameListPage.jsx
@@ -7,7 +7,7 @@ function AdminGameListPage() {
   return (
     <div>
       <div
-        className="ml-40 items-center bg-indigo-400 leading-none rounded-full py-3 px-5 text-teal
+        className="ml-40 items-center bg-indigo-400 leading-none rounded-full py-3 px-5 text-teal border border-indigo-400
           font-semibold cursor-pointer relative inline-block justify-center
           hover:bg-white  hover:border hover:border-indigo-400
           text-white text-lg hover:text-indigo-400
