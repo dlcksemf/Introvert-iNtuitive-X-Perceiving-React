58ede8e44f121122a29088a48d50211344a247e2
Jina Kim <fghjiurt@naver.com>
2022-04-17 20:44:52 +0900

    토스트 수정

--- src/components/parts/LoginToast.jsx
+++ src/components/parts/LoginToast.jsx
@@ -5,14 +5,14 @@ function LoginToast() {
 
   return (
     <div>
-      <div>📘 로그인 창으로 이동하시겠습니까?</div>
+      <div>로그인 창으로 이동하시겠습니까?</div>
 
       <button
         onClick={() => {
           navigate('/accounts/login/');
         }}
       >
-        이동하기!!
+        <h1 className="ml-24 text-sky-600 font-bold">이동하기</h1>
       </button>
     </div>
   );
